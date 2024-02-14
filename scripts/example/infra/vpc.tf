2d3700415e7c8036d297bd55c3d481a526a5961f
Hyeonmin Kim <khm970514@gmail.com>
2024-02-14 13:44:17 +0900

    Update : IaC (Terraform)

---
+++ scripts/example/infra/vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "hmkim-tf-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+  enable_vpn_gateway     = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  tags = {
+    Terraform = "true"
+  }
+}
